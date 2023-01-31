05ca45ca8e9c0839a05dec3b3dcf61112e075951
tleanne1 <114450881+tleanne1@users.noreply.github.com>
2023-01-30 21:39:49 -0600

    Updated CSS

--- src/app.js
+++ src/app.js
@@ -47,7 +47,7 @@ function displayForecast(response) {
             forecastDay.weather[0].icon
           }@2x.png"
           alt=""
-          width="40"
+          width="80"
         />
         <div class="weather-forecast-temperatures">
           <span class="weather-forecast-temperature-max"> ${Math.round(
